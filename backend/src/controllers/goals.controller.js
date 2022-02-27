ee629d7b9ac107376986298cc7879f62149b08ec
matsubrasil <matsuboy@outlook.com>
2022-02-27 13:24:26 -0300

    feat: add express-async-handler

--- backend/src/controllers/goals.controller.js
+++ backend/src/controllers/goals.controller.js
@@ -1,30 +1,32 @@
-const getGoals = (req, res) => {
-  res.status(200).json({ message: 'Api Goals' });
-};
+const asyncHandler = require('express-async-handler');
 
-const getOneGoal = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Api Goals' });
+});
+
+const getOneGoal = asyncHandler(async (req, res) => {
   const id = req.params.id;
   res.status(200).json({ message: `Get Goal: ${id}` });
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   const { text } = req.body;
   if (!text) {
     res.status(400);
     throw new Error('Please add a text');
   }
   res.status(201).json({ message: `POST goals: ${text}` });
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   const id = req.params.id;
   res.status(200).json({ message: `Update Goals: ${id}` });
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   const id = req.params.id;
   res.status(200).json({ message: `Delete Goals: ${id}` });
-};
+});
 
 module.exports = {
   getGoals,
