1aa5978d17b6c68a84203b71a8ed89a03ca295c1
matsubrasil <matsuboy@outlook.com>
2022-03-01 18:18:45 -0300

    feat: dashboard redirect to login

--- frontend/src/pages/dashboard.page.jsx
+++ frontend/src/pages/dashboard.page.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
+  return <div>Dashboard</div>;
+};
