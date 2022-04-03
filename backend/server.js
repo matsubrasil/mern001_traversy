29ca4764ec04011fafe954dbf3d7a65a7c48460d
matsubrasil <matsuboy@outlook.com>
2022-04-03 10:41:09 -0300

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -19,6 +20,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', routesGoals);
 app.use('/api/users', routesUsers);
+
+// Server handler
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) =>
+    res.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
 
